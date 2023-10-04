cbc6d6aaf8f7b14e425d195df9cccf8a5be6ca2b
Will Stone <willstone@Wills-MBP.cable.rcn.com>
2023-10-04 16:17:23 -0400

    fixing deploy issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import { Router } from './Router';
 import { theme } from './theme';
+import { HomePage } from './pages/Home.page';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Router />
+    <MantineProvider defaultColorScheme="dark">
+      <HomePage />
     </MantineProvider>
   );
 }
