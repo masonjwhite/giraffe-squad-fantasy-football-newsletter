825643946244cc392353c977fb1339bd61a18225
Mason J. White <mju285@wildcats.unh.edu>
2024-08-31 16:19:13 -0400

    Multiple-weeks: - Clearing out dummy data, getting ready for Week 1

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import Root from './routes/root';
 import Week1 from './routes/weeks/week1';
-import Week2 from './routes/weeks/week2';
-import Week3 from './routes/weeks/week3';
 import NotFound from './routes/notFound/notFound';
 // import { theme } from './theme';
 
@@ -20,14 +18,6 @@ const router = createBrowserRouter([
             {
                 path: 'weeks/1',
                 element: <Week1 />,
-            },
-            {
-                path: 'weeks/2',
-                element: <Week2 />,
-            },
-            {
-                path: 'weeks/3',
-                element: <Week3 />,
             },
         ],
     },
