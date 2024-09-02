48ace910c0289355d88e133a9fd73c90b50832b8
willlstone <stonew2@rpi.edu>
2024-09-02 12:35:39 -0400

    util to create weekly file

--- README.md
+++ README.md
@@ -1,34 +1,38 @@
 # Giraffe Squad Fantasy Football Newsletter
 
 ## Pre-requisites
+
 - 💻 Node.js v21
 - 🏈 A passion for Fantasy Football
 - 🤣 A good sense of humor and creative spirit
 
 ## Local setup
+
 1. Clone the repository
 2. Change into the created project directory and run `npm install`
-3. Run `npm run dev` to start the project 
+3. Run `npm run dev` to start the project
 4. The project should be accessible at http://localhost:5173
 
 ## Folder structure
+
 - `src/components` - Reusable components that are utilized in various pages
-- `src/data` - The data that will be used to populate content for a week. 
-    - **IMPORTANT: Each week should have its own unique data file.**
+- `src/data` - The data that will be used to populate content for a week.
+  - **IMPORTANT: Each week should have its own unique data file.**
 - `src/hooks` - React hooks that are used throughout the project
 - `src/images` - Various images that are used throughout the project
 - `src/routes` - The various pages for the site.
-    - **IMPORTANT: Each week should have its own unique route file.**
+  - **IMPORTANT: Each week should have its own unique route file.**
 - `src/global.module.css` - Reusable global styles that are used throughout the site
 - `src/settings.tsx` - Global settings that are used throughout the site
 - `src/types.ts` - Global TypeScript types that are used throughout the site
 
 ## Adding a new week
+
 To add a new week to the site, please follow the steps below.
 
-1. Create a new week file under `src/routes/weeks` with the name of the week
-2. Customize the week file to include any combination of components you'd like. The only required components are: `Title`, `WeekSelector` and `WeekNav` (see previous weeks for usage example), however the `Scores` and `Standings` are strongly recommended.
-3. Create a new data file under `src/data` with the name of the week and add the data that will be needed by the components on your page.
+1. Make sure you have added an `APP_KEY` and `APP_SECRET` to your `.env`
+2. Modify the `create-weekly-file` script in the `package.json` to take the week of the file you want to create. (the number after `createDataFile.js`)
+3. Run the `create-weekly-file` script
 4. To see your page in the site, add your route component and route name in the `src/main.tsx` file.
 5. Once ready to publish, change the `CURRENT_PUBLISHED_WEEK` value to your new week in the `src/settings.ts` file.
 6. All routing and navigation should be automatically handled for you thanks to some pretty cheeky component design if I do say so myself 😜
---
+++ api/createdata.js
@@ -0,0 +1,111 @@
+const YahooFantasy = require('yahoo-fantasy');
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Week is the first argument passed in the command line
+// ex: node createdata.js 3
+// this will create a file for week 3
+const week = process.argv.slice(2)[0];
+
+const app_key = process.env.APP_KEY;
+const app_secret = process.env.APP_SECRET;
+const giraffe_league_key = '449.l.322781';
+
+const yf = new YahooFantasy(
+  app_key,
+  app_secret
+);
+
+const imports = `
+import { Text } from '@mantine/core';
+import { Team, Matchup, Article, Player } from '../types';
+import Jersey from '../images/Giraffe_Jersey.png';
+import GiraffeHelmet from '../images/Giraffe_Helmet.webp';\n\n`;
+
+const fileStart = "export const teams: Team[] = [\n";
+const endTeamsData = "];\n";
+let matchupsHeader = `\nexport const matchups: Matchup[] = [\n`;
+
+const articlePlaceholder = `
+export const article: Article = {
+  coverImage: Jersey,
+  title: 'TBD',
+  summary: 'Some summary',
+  author: 'Some author',
+  content: <Text>PLACEHOLDER</Text>,
+};
+
+export const jokeArticles: string[] = ['TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD'];
+
+export const playerOfTheWeek: Player = {
+  name: 'Dak Prescott',
+  points: 31,
+};
+`
+const coaches = {
+  1: "Mason White",
+  2: "Kara Prescott",
+  3: "Scott Quinn",
+  4: "Zac Fermanis",
+  5: "Will Stone",
+  6: "Zari Bazarian",
+  7: "Adam LaCasse",
+  8: "Jackie Munger",
+  9: "Joe Pecosky",
+  10: "Nick Bradfish",
+  11: "Jaime Sheppard",
+  12: "Marc Bouvier"
+}
+
+async function getAllFileData() {
+  try {
+    let teams = await yf.league.standings(giraffe_league_key);
+    const teamData = teams.standings.map((team) => {
+      return `{
+        id: "${team.team_id}",
+        logo: "${team.team_logos[0].url}",
+        name: "${team.name}",
+        coach: "${coaches[team.team_id]}",
+        record: "${team.standings.outcome_totals.wins}-${team.standings.outcome_totals.losses}",
+        pointsFor: ${team.standings.points_for},
+        pointsAgainst: ${team.standings.points_against},
+        powerRank: 0,
+        powerRankText: "",
+        helmetImage: undefined,
+        draftGrade: "${team.draft_grade}"
+      }
+        `;
+    }).join(',\n');
+
+  let scoreboard = await yf.league.scoreboard(giraffe_league_key, week);
+  const scoreData = scoreboard.scoreboard.matchups.map((matchup) => {
+    return `{\n
+      homeTeam: {
+        name: "${matchup.teams[1].name}",
+        id: "${matchup.teams[1].team_id}",
+        score: ${matchup.teams[1].points.total},
+      },
+      awayTeam: {
+        name: "${matchup.teams[0].name}",
+        id: "${matchup.teams[0].team_id}",
+        score: ${matchup.teams[0].points.total},
+      }
+    }`;
+  });
+
+  return imports + fileStart + teamData + endTeamsData + matchupsHeader + scoreData.join(',\n') + endTeamsData + articlePlaceholder;
+  } catch (e) {
+    console.log('error:', e);
+  }
+}
+
+getAllFileData().then((fileText) => {
+   fs.writeFile(`src/data/week${week}output.tsx`, fileText, (err) => {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      console.log('File created successfully');
+    }
+  });
+});
